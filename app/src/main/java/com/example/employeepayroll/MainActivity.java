2f244c7cb67380a12b69a0deafa3969b99d00f9c
anmol sharma <anmolsharma433@gmail.com>
2019-12-06 18:08:32 -0500

    login page done

--- app/src/main/java/com/example/employeepayroll/MainActivity.java
+++ app/src/main/java/com/example/employeepayroll/MainActivity.java
@@ -1,14 +1,93 @@
 package com.example.employeepayroll;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView banner;
+    EditText email;
+    EditText password;
+    Button login;
+    TextView dm;
+    Switch sw;
+    //Id for layout
+    ConstraintLayout cl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        banner = findViewById(R.id.txtBanner);
+        email = findViewById(R.id.etEmail);
+        password = findViewById(R.id.etPassword);
+        login = findViewById(R.id.btnLogin);
+        cl = findViewById(R.id.clLayout);
+
+        //setting the values at startUp
+        email.setText("user@employee.com");
+        password.setText("s3cr3t");
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (email.getText().toString().trim().length() != 0) {
+
+                    if (email.getText().toString().equals("user@employee.com") && password.getText().toString().equals("s3cr3t")) {
+                        Toast.makeText(MainActivity.this, "You are loggedin.", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Your email id or password is incorrect.", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    alertDialog();
+                }
+            }
+        });
+
+    }
+
+
+    private void alertDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Login Error");
+        dialog.setMessage("Please Enter the required Fields");
+        dialog.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog alertDialog = dialog.create();
+        alertDialog.show();
     }
 }
+
+
